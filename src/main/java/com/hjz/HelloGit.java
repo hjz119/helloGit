50b59fcc230d9cae86e21523b64df5e68e7c8f85
hjz <huangjunzhe119@gmail.com>
2021-04-13 18:32:12 +0800

    branch conflict

--- src/main/java/com/hjz/HelloGit.java
+++ src/main/java/com/hjz/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("create branch");
+        System.out.println("branch conflict");
     }
 }
