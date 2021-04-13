6090834aaa47903df4cd14f84ca6dc7f6fa3b7dd
hjz <huangjunzhe119@gmail.com>
2021-04-13 18:27:12 +0800

    create branch

--- src/main/java/com/hjz/HelloGit.java
+++ src/main/java/com/hjz/HelloGit.java
@@ -3,6 +3,6 @@ package com.hjz;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
-
+        System.out.println("create branch");
     }
 }
